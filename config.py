2a6fd2fd0f0f7dcd9a4e78d56228ccbc30eb5529
HipyCas <hipycas@gmail.com>
2020-08-17 00:36:59 +0200

    Update config.py
    Added properties for forms and sqlalchemy, support for .env files, and a random string generator for the secret key if none is provided

--- config.py
+++ config.py
@@ -1,2 +1,17 @@
-class Config:
-    pass
+import os
+from dotenv import load_dotenv
+import uuid
+
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '.env'))
+
+random_key = uuid.uuid4().hex
+
+
+class Config (object):
+    SECRET_KEY = os.environ.get('SECRET_KEY') or random_key
+
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+                              'sqlite:///' + os.path.join(basedir, 'welex.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
