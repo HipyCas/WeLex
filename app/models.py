ae86a7de63f50f2b8eb71adf4b3cba297443dae7
HipyCas <hipycas@gmail.com>
2020-08-18 23:54:13 +0200

    Update models.py

--- app/models.py
+++ app/models.py
@@ -2,13 +2,12 @@
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 
-from app import db
+from app import db, login
 
 
-"""roles_users_table = db.Table('roles_users',
-								db.Column('users_id', db.Integer, db.ForeignKey('users.id')),
+roles_users_table = db.Table('roles_users',
+								db.Column('users_id', db.Integer, db.ForeignKey('user.id')),
 								db.Column('roles_id', db.Integer, db.ForeignKey('roles.id')))
-"""
 
 
 class RegistrationToken(db.Model):
@@ -29,16 +28,14 @@ class RegistrationToken(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	token = db.Column(db.String, unique=True, index=True)
 	target_name = db.Column(db.String, unique=True, index=True)
-	target_id = db.Column(db.Integer, db.ForeignKey('users.id'))
-	#target = db.relationship("User", uselist=False, back_populates="registration_token", foreign_keys=[user.registration_token_id])
 
 
 class User(db.Model, UserMixin):
-	__tablename__ = 'users'
+	__tablename__ = 'user'
 
 	id = db.Column(db.Integer, primary_key=True)
 	active = db.Column(db.Boolean)
-	#roles = db.relationship('Role', secondary=roles_users_table, backref='user', lazy='dynamic')
+	roles = db.relationship('Role', secondary=roles_users_table, backref='user', lazy='dynamic')
 	alias = db.Column(db.String(32), unique=True, index=True)
 	email = db.Column(db.String(140), unique=True, index=True)
 	nombre = db.Column(db.String(24))
@@ -52,24 +49,22 @@ class User(db.Model, UserMixin):
 	def check_password(self, check):
 		return check_password_hash(self.password_hash, check)
 
-
-#RegistrationToken.dispatcher_id = db.Column(db.Integer, db.ForeignKey('users.id'))
-#User.registration_tokens = db.relationship('RegistrationToken', backref='dispatcher', lazy='dynamic', foreign_keys=[RegistrationToken.dispatcher_id], primaryjoin=(RegistrationToken.dispatcher_id==User.id))
-
 """
+RegistrationToken.dispatcher_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+User.registration_tokens = db.relationship('RegistrationToken', backref='dispatcher', lazy='dynamic', foreign_keys=[RegistrationToken.dispatcher_id], primaryjoin=(RegistrationToken.dispatcher_id==User.id))
+
 RegistrationToken.target = db.relationship("User", uselist=False, back_populates="registration_token", foreign_keys=[User.registration_token_id])
-RegistrationToken.target_id = db.Column(db.Integer, db.ForeignKey('users.id'))
-User.registration_token = db.relationship('RegistrationToken', back_populates="user", foreign_keys=[RegistrationToken.target], primaryjoin='RegistrationToken.target_id==User.registration_token_id')
+RegistrationToken.target_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+User.registration_token = db.relationship('RegistrationToken', back_populates="user", foreign_keys=[RegistrationToken.target], primaryjoin=(RegistrationToken.target_id==User.registration_token_id))
 """
 
-"""
 class Role(db.Model):
 	__tablename__ = 'roles'
 
 	id = db.Column(db.Integer, primary_key=True)
 	name = db.Column(db.String(32), unique=True, index=True)
 	description = db.Column(db.String(140))
-"""
+
 
 class Expediente(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
@@ -100,15 +95,13 @@ class Actuacion(db.Model):
 
 class Evento(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
-	autor = db.Column(db.Integer, db.ForeignKey('users.id'))
+	autor = db.Column(db.Integer, db.ForeignKey('user.id'))
 	actuacion = db.Column(db.Integer, db.ForeignKey('actuacion.id'))
 	visibilidad = db.Column(db.Integer)  # 0 -> publico; 1 -> privado
 	tipo = db.Column(db.String(12))  # TODO relationship new TipoEvento model
 	contenido = db.Column(db.String(160))
 
 
-"""
 @login.user_loader
 def load_user(id):
 	return User.query.get(int(id))
-"""
