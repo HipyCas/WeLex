507e2338fdf1dabe48864403f99b1ab999c57b83
HipyCas <hipycas@gmail.com>
2020-08-22 20:20:08 +0200

    Admin required decorator
    Added an admin required decorator that checks wether the user is marked as admin and aborts with a 403 if it is not.

--- create user.py
+++ create user.py
@@ -1,14 +1,23 @@
 from app import create_app, db
 from app.models import User
 
+app = create_app()
+app.app_context().push()
+
+if bool(input('Set password (True) or create user (False): ')):
+    id = str(input('Id: '))
+    u = User.query.get(id)
+    u.set_password(str(input('Password to hash: ')))
+    db.session.commit()
+    exit(0)
 
 name = str(input('Name: '))
 username = str(input('Username: '))
 email = str(input('Email: '))
 password = str(input('Password: '))
+admin = bool(input('Admin (True/False): '))
 
-create_app().app_context().push()
-u = User(alias=username, email=email, nombre=name)
+u = User(alias=username, email=email, nombre=name, admin=admin, active=True)
 u.set_password(password)
 db.session.add(u)
 db.session.commit()
