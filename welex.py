b8d9a1567c0bb61b88fbfc4dd066116941ff62b8
HipyCas <hipycas@gmail.com>
2020-08-24 01:30:11 +0200

    Register cli & shell context
    Added the cli.register(app)  instruction to the  main file and the shell context processors

--- welex.py
+++ welex.py
@@ -1,6 +1,11 @@
-
-from app import create_app
+from app import create_app, cli, db, mail
+from app.models import RegistrationToken, User, Expediente, Actuacion, Evento
 
 
 app = create_app()
+cli.register(app)
 
+
+@app.shell_context_processors
+def make_shell_context():
+    return {'app': app, 'db': db, 'mail': mail, 'RegistrationToken': RegistrationToken, 'User': User, 'Expediente': Expediente, 'Actuacion': Actuacion, 'Evento': Evento}
