6bbd2210e035c6fc0929d607f6cf3ccc9708f581
HipyCas <hipycas@gmail.com>
2020-10-07 00:20:35 +0200

    íº©Added classes query
    Added lines that, with JQuery, query the following classes:
    form-required, form-password, form-email, form-submit.
    Added console.log entries after each query to print it to the console.

--- app/static/js/form-validate.js
+++ app/static/js/form-validate.js
@@ -3,5 +3,12 @@ console.log(">> Script called for first time");
 $(document).ready(function(){
 	console.log('>> Form validation script started');
 
+	// Get form components
+	console.log("> Getting elements");
+	var required = $(".form-required"); console.log("required: " + required);
+	var password = $(".form-password"); console.log("Password: " + password);
+	var email = $(".form-email"); console.log("Email: " + email);
+    var submit = $(".form-submit"); console.log("Submit: " + submit);
+
 	console.log('>> Form validation script finished');
 });
