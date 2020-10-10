c0b178cd11d09f80f19f487dace06e8df3392dee
HipyCas <hipycas@gmail.com>
2020-10-10 14:18:28 +0200

    🔐 Started work on password validation
    Created the RegExp for the password match cases.
    Tried to implement the regex and validation but didn't work.

--- app/static/js/form-validate.js
+++ app/static/js/form-validate.js
@@ -3,8 +3,18 @@ console.log(">> Script called for first time");
 $(document).ready(function(){
 	console.log('>> Form validation script started');
 
-	// Create regex's
+	// Email regex
 	const emailRegex = /[a-zA-Z0-9]+@[a-zA-Z0-6]{2,}.(com|co|es|en|fr|it|pt|ir|bg|nl|no|au|ch|de|sw|gl|gal|int|org|edu)/
+
+	// Password regex's
+	const lowerCase = /[a-z]+/;
+	const upperCase = /[A-Z]+/;
+	const number = /[0-9]/;
+	const special = /[$%@&*]/;
+	const lettersReg = /[a-z]/mig;
+	const lettersEntropy = Math.log2(26); console.log(lettersEntropy);
+	const numberSpecialReg = /[0-9$@%&*]/mig;
+	const numberSpecialEntropy = Math.log2(94); console.log(numberSpecialEntropy);
 
 	// Get form components
 	console.log("> Getting elements");
@@ -54,6 +64,7 @@ $(document).ready(function(){
     	});
 	});
 	
+	// Set email validation
 	email.each(function() {
 		$(this).on('input', function(event) {
 			if (!emailRegex.test($(this).val())) {
@@ -77,5 +88,51 @@ $(document).ready(function(){
 		});
 	});
 
+	password.each(function() {
+		$(this).on('input', function() {
+			console.log('Password changed')
+			// Set drop password tips status
+			console.log('Lower case: ' + lowerCase.test($(this).val()));
+			let lowerCaseIcon = $(this).closest('.form-password--drop').children('pre').children('.lowercaseIcon');
+			if (lowerCase.test($(this).val())) {
+				lowerCaseIcon.attr('uk-icon', 'check');
+			} else {
+				lowercaseIcon.attr('uk-icon', 'close');
+			}
+			//_ = (lowerCase.test($(this).val())) ? lowerCaseIcon.attr('uk-icon', 'check') : lowercaseIcon.attr('uk-icon', 'close');
+			upperCase.test($(this).val()) ? $(this).closest('.uppercaseIcon').attr('uk-icon', 'check') : $(this).closest('.uppercaseIcon').attr('uk-icon', 'close');
+			number.test($(this).val()) ? $(this).closest('.numberIcon').attr('uk-icon', 'check') : $(this).closest('.numberIcon').attr('uk-icon', 'close');
+			special.test($(this).val()) ? $(this).closest('.specialIcon').attr('uk-icon', 'check') : $(this).closest('.specialIcon').attr('uk-icon', 'close');
+			// Record entropy points
+			let points = 0;
+			console.log(lettersReg.exec($(this).val()));
+			if (lettersReg.exec($(this).val()) != null) {
+				points += lettersReg.exec($(this).val()).length * lettersEntropy;
+			}
+			if (numberSpecialReg.exec($(this).val()) != null) {
+				points += numberSpecialReg.exec($(this).val()).length * numberSpecialEntropy;
+			}
+			console.log('Password points: ' + points + ' (for password: ' + $(this).val() + ')');
+			let progressBar = $(this).closest('.form-password--progress');
+			if (points < 30) {
+				progressBar.addClass('uk-text-danger');
+				progressBar.attr('value', '1');
+			} else if (points < 50) {
+				progressBar.addClass('uk-text-warning');
+				progressBar.attr('value', '2');
+			} else if (points < 75) {
+				progressBar.addClass('uk-tex-primary');
+				progressBar.attr('value', '3');
+			} else {
+				progressBar.addClass('uk-text-success');
+				progressBar.attr('value', '4');
+			}
+		})
+	});
+
 	console.log('>> Form validation script finished');
 });
+
+function passwordEntropy (password) {
+	let letters = lettersReg.match(password) * lettersEntropy;
+}
