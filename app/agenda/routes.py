752f765e1f4734b69a82ed1b6c58b2e6af3ece6e
HipyCas <hipycas@gmail.com>
2020-12-16 18:43:06 +0100

    🗑 Added mock event delete
    Added a route for a deleting events, which as of now only flashes the id and redirects.

--- app/agenda/routes.py
+++ app/agenda/routes.py
@@ -1,5 +1,6 @@
-from flask import render_template, request
+from flask import render_template, request, url_for, redirect, flash
 from flask_babel import _
+from werkzeug.urls import url_parse
 from datetime import datetime
 
 from app.decorators import active_required
@@ -12,3 +13,14 @@ def start():
     date = request.args.get('date', datetime.today().strftime('%Y-%m-%d'), str)
     # TODO: query event by date `date`
     return render_template('agenda.html', title=_('Agenda'), date=date)
+
+
+event_delete_route = '/delete/<id>'
+@bp.route(event_delete_route)
+@active_required
+def event_delete(id):
+    # TODO Redirect to passed url
+    flash(f'Deleted event {id} successfully', 'success')
+    # Redirect to passed url
+    next_page = request.args.get('next')
+    return redirect(next_page)
